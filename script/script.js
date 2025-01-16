64553f590fa9a593f6fd2ba8ca85a35cb3576617
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2025-01-16 11:17:20 +0200

    new work

--- script/script.js
+++ script/script.js
@@ -1,31 +1,59 @@
-const emojis = ["😊","😊","😂","😂","😎","😎","😁","😁","🤣","🤣","😃","😃","😆","😆","👀","👀"]
-let shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1)
+const emojis = [
+  "😊",
+  "😊",
+  "😂",
+  "😂",
+  "😎",
+  "😎",
+  "😁",
+  "😁",
+  "🤣",
+  "🤣",
+  "😃",
+  "😃",
+  "😆",
+  "😆",
+  "👀",
+  "👀",
+];
+let shuf_emojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 for (let i = 0; i < emojis.length; i++) {
-  let box = document.createElement('div')
-  box.className = 'man'
-  box.innerHTML = shuf_emojis[i]
+  let box = document.createElement("div");
+  box.className = "man";
+  box.innerHTML = shuf_emojis[i];
 
-  box.onclick = function(){
-    this.classList.add('box-open')
-    setTimeout(function(){
-      if(document.querySelectorAll('.box-open').length > 1){
-        if (document.querySelectorAll('.box-open')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
-          document.querySelectorAll('.box-open')[0].classList.add('box-match')
-          document.querySelectorAll('.box-open')[1].classList.add('box-mtch')
+  box.onclick = function () {
+    this.classList.add("box-open");
+    setTimeout(function () {
+      if (document.querySelectorAll(".box-open").length > 1) {
+        if (
+          document.querySelectorAll(".box-open")[0].innerHTML ==
+          document.querySelectorAll(".boxOpen")[1].innerHTML
+        ) {
+          document.querySelectorAll(".box-open")[0].classList.add("box-match");
+          document.querySelectorAll(".box-open")[1].classList.add("box-mtch");
 
-          document.querySelectorAll('.box-open')[1].classList.remove('box-open')
-          document.querySelectorAll('.box-open')[0].classList.remove('box-open')
+          document
+            .querySelectorAll(".box-open")[1]
+            .classList.remove("box-open");
+          document
+            .querySelectorAll(".box-open")[0]
+            .classList.remove("box-open");
 
-          if(document.querySelectorAll('.box-oatch').length == emojis.length){
-            alert('Congratulaitions🎈🎉 you made it to the Top')
+          if (document.querySelectorAll(".box-oatch").length == emojis.length) {
+            alert("Congratulaitions🎈🎉 you made it to the Top");
           }
         } else {
-          document.querySelectorAll('.box-open')[1].classList.remove('box-open')
-          document.querySelectorAll('.box-open')[0].classList.remove('box-open')
+          document
+            .querySelectorAll(".box-open")[1]
+            .classList.remove("box-open");
+          document
+            .querySelectorAll(".box-open")[0]
+            .classList.remove("box-open");
+        }
       }
-      }
-    },500)
-  }
+    }, 500);
+  };
 
-  document.querySelector('.game').appendChild(box)
+  document.querySelector(".game").appendChild(box);
 }
