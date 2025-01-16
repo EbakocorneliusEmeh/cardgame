283bd691ea13b38614f8dc10dcf4a24ac84422c7
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2025-01-16 11:03:37 +0200

    new work

--- script/script.js
+++ script/script.js
@@ -6,22 +6,22 @@ for (let i = 0; i < emojis.length; i++) {
   box.innerHTML = shuf_emojis[i]
 
   box.onclick = function(){
-    this.classList.add('boxOpen')
+    this.classList.add('box-open')
     setTimeout(function(){
-      if(document.querySelectorAll('.boxOpen').length > 1){
-        if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
-          document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
-          document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+      if(document.querySelectorAll('.box-open').length > 1){
+        if (document.querySelectorAll('.box-open')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
+          document.querySelectorAll('.box-open')[0].classList.add('box-match')
+          document.querySelectorAll('.box-open')[1].classList.add('box-mtch')
 
-          document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
-          document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+          document.querySelectorAll('.box-open')[1].classList.remove('box-open')
+          document.querySelectorAll('.box-open')[0].classList.remove('box-open')
 
-          if(document.querySelectorAll('.boxMatch').length == emojis.length){
+          if(document.querySelectorAll('.box-oatch').length == emojis.length){
             alert('Congratulaitions🎈🎉 you made it to the Top')
           }
         } else {
-          document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
-          document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+          document.querySelectorAll('.box-open')[1].classList.remove('box-open')
+          document.querySelectorAll('.box-open')[0].classList.remove('box-open')
       }
       }
     },500)
